60a735cd56de240baaac5b79f38e92d3a10a0bfd
Rob Zinkov <rob@zinkov.com>
2011-01-12 02:07:13 -0800

    Filled out project and README

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-stemmer "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.zaxtax/libstemmer "0.1"]])
---
+++ src/clj_stemmer/core.clj
@@ -0,0 +1,1 @@
+(ns clj-stemmer.core)

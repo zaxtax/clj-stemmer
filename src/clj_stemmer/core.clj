048e94d712897b2eb23c59ba1113cdb5b051dd41
Rob Zinkov <rob@zinkov.com>
2011-01-12 02:14:03 -0800

    Explicitly list possible stemmers in source code

--- src/clj_stemmer/core.clj
+++ src/clj_stemmer/core.clj
@@ -1,5 +1,19 @@
 (ns clj-stemmer.core
   (:import (org.tartarus.snowball.ext englishStemmer
+				      danishStemmer
+				      frenchStemmer
+				      hungarianStemmer
+				      norwegianStemmer
+				      portugueseStemmer
+				      russianStemmer
+				      swedishStemmer
+				      dutchStemmer
+				      finnishStemmer
+				      germanStemmer
+				      italianStemmer
+				      romanianStemmer
+				      spanishStemmer
+				      turkishStemmer
 				      porterStemmer)))
 (defn stem [word]
   (let [stemmer (doto (englishStemmer.)
